b5841c90e7437d02b82395bb7a1d7fa91cb5c04f
justinmann <justinmann24@gmail.com>
2022-08-23 21:33:12 -0400

    8/23 updates

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { productsApiSlice } from './api/productsApiSlice';
-import { ordersApiSlice } from './api/ordersApiSlice';
-import { authApiSlice } from './api/authApiSlice';
-import { customersApiSlice } from './api/customersApiSlice';
-
-store.dispatch(productsApiSlice.endpoints.getProducts.initiate());
-store.dispatch(productsApiSlice.endpoints.getProduct.initiate());
-store.dispatch(ordersApiSlice.endpoints.getStoreOrders.initiate());
-store.dispatch(authApiSlice.endpoints.getUpdatedUser.initiate());
-store.dispatch(authApiSlice.endpoints.getOnboardUrl.initiate());
-store.dispatch(customersApiSlice.endpoints.getCustomers.initiate());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
