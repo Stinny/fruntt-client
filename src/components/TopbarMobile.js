9f2263b67347daff474982973b3b55ae5690fc50
justinmann <justinmann24@gmail.com>
2022-11-03 17:14:23 -0400

    fixed scrolling issue on topbar

--- src/components/TopbarMobile.js
+++ src/components/TopbarMobile.js
@@ -13,6 +13,7 @@ const TopbarMobile = () => {
         <div className='w-11/12 h-full mx-auto flex justify-between items-center border-b-2 p-2 overflow-x-scroll'>
           <NavLink to='/dashboard'>
             <button
+              autoFocus={path === '/dashboard'}
               className={path === '/dashboard' ? activeLink : notActiveLink}
             >
               Home
@@ -21,10 +22,11 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/item'>
             <button
+              autoFocus={
+                path === '/dashboard/item' || path === '/dashboard/item/add'
+              }
               className={
-                path === '/dashboard/item' ||
-                path === '/dashboard/item/add' ||
-                path === '/dashboard/item/add/buynow'
+                path === '/dashboard/item' || path === '/dashboard/item/add'
                   ? activeLink
                   : notActiveLink
               }
@@ -35,6 +37,10 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/design'>
             <button
+              autoFocus={
+                path === '/dashboard/design' ||
+                path === '/dashboard/design/edit'
+              }
               className={
                 path === '/dashboard/design' ||
                 path === '/dashboard/design/edit'
@@ -48,6 +54,7 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/content'>
             <button
+              autoFocus={path === '/dashboard/content'}
               className={
                 path === '/dashboard/content' ? activeLink : notActiveLink
               }
@@ -58,11 +65,9 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/orders'>
             <button
+              autoFocus={path === '/dashboard/orders'}
               className={
-                path === '/dashboard/orders' ||
-                path === '/dashboard/orders/:orderId'
-                  ? activeLink
-                  : notActiveLink
+                path === '/dashboard/orders' ? activeLink : notActiveLink
               }
             >
               Orders
@@ -71,6 +76,7 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/customers'>
             <button
+              autoFocus={path === '/dashboard/customers'}
               className={
                 path === '/dashboard/customers' ? activeLink : notActiveLink
               }
@@ -91,6 +97,7 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/addons'>
             <button
+              autoFocus={path === '/dashboard/addons'}
               className={
                 path === '/dashboard/addons' ? activeLink : notActiveLink
               }
@@ -101,6 +108,7 @@ const TopbarMobile = () => {
 
           <NavLink to='/dashboard/config'>
             <button
+              autoFocus={path === '/dashboard/config'}
               className={
                 path === '/dashboard/config' ? activeLink : notActiveLink
               }
