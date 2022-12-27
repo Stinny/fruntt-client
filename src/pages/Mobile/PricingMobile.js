81d8be790898f1a64900fcb2ed8905b79b22e25f
justinmann <justinmann24@gmail.com>
2022-12-26 21:54:31 -0500

    updates

--- src/pages/Mobile/PricingMobile.js
+++ src/pages/Mobile/PricingMobile.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { RiSecurePaymentFill } from 'react-icons/ri';
 import { MdMobileFriendly } from 'react-icons/md';
 import { GrHostMaintenance, GrStripe } from 'react-icons/gr';
-import { FaEarlybirds } from 'react-icons/fa';
+import { FaLongArrowAltDown } from 'react-icons/fa';
 
 //mui
 import Alert from '@mui/material/Alert';
@@ -21,43 +21,45 @@ const PricingMobile = () => {
   return (
     <div className='w-full mx-auto h-fit'>
       <div className='w-full mx-auto flex flex-col items-center p-2'>
-        <p className='text-3xl font-medium mt-8'>Pricing</p>
-        <Alert severity='info' className='mt-2'>
-          Welcome merchants! We are very excited to finally be onboarding users.
-          Please enjoy our platform for FREE while we collect feedback and
-          develop a pricing plan.
-        </Alert>
+        <p className='text-3xl font-medium mt-8 border-b'>Pricing</p>
+        <div className='w-full mt-4 flex flex-col'>
+          <div className='border-2 rounded w-full flex items-center justify-center h-24'>
+            <p className='text-2xl font-medium text-slate-800'>
+              First product page FREE
+            </p>
+          </div>
+          <div className='w-full flex flex-col items-center justify-center mt-2 mb-2'>
+            <p className='font-medium'>additional pages</p>
+            <FaLongArrowAltDown className='text-3xl' />
+          </div>
+          <div className='border-2 rounded w-full flex flex-col items-center justify-center p-2'>
+            <p className='font-medium text-4xl'>
+              <span className='text-4xl'>$3</span>/mo
+            </p>
+            <p className='font-medium text-gray-400'>
+              Per additional product page
+            </p>
+          </div>
+        </div>
 
         <div className='border-2 rounded w-full h-full p-4 mt-4'>
-          {/* <div className='flex items-center mb-2'>
-            <p className='text-2xl font-medium'>Early bird special</p>
-            <FaEarlybirds className='text-2xl' />
-          </div> */}
-          <p className='font-medium text-3xl'>
-            <span className='text-4xl'>TBD</span>/mo
-          </p>
-          <p className='font-medium text-gray-400 text-lg'>Per product page</p>
           <p className='mt-4 text-xl text-slate-800 font-medium'>
             Platform includes:
           </p>
           <div className='w-full text-slate-800 text-lg p-2'>
             <p className='mt-2'>
-              - Easily launch, design, and manage multiple product pages
+              - Easily launch, design, and manage product pages
             </p>
-            <p className='mt-4'>- Import products</p>
+            <p className='mt-4'>- Offer physical or digital goods</p>
             <p className='mt-4'>
               - Get shipping labels & rates directly from us
             </p>
+            <p className='mt-4'>- No transaction fees</p>
             <p className='mt-4'>
-              - Automated customer emailing (order confirmation, order shipped,
-              etc.)
+              - Automated customer emailing and order tracking
             </p>
-            <p className='mt-4'>- Order tracking for customers</p>
-
             <p className='mt-4'>- Collect reviews from customers</p>
-
-            <p className='mt-4'>- Connect your own domain</p>
-            <p className='mt-4'>- 24/7 email support</p>
+            <p className='mt-4'>- Connect your own domain to pages</p>
           </div>
           <Link to='/signup' className='w-full'>
             <button className='w-full mt-4 h-14 text-lg border-2 border-slate-800 text-slate-800 rounded hover:text-white hover:bg-slate-800'>
@@ -79,14 +81,14 @@ const PricingMobile = () => {
             <MdMobileFriendly className='text-3xl' />
             <p className='font-medium text-xl mt-2'>100% Mobile friendly</p>
             <p className='mt-2 text-lg'>
-              All pages are mobile friendly, never miss a mobile customer
+              All pages are mobile friendly, never miss out on a mobile customer
             </p>
           </div>
           <div className='w-full border-2 mt-2 rounded p-2'>
             <GrHostMaintenance className='text-3xl' />
             <p className='font-medium text-lg mt-2'>Hosted by us</p>
             <p className='mt-2 text-lg'>
-              Don't ever worry about hosting your page, we got you
+              Never worry about hosting, pages are hosted and maintained by us
             </p>
           </div>
         </div>
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -7,7 +7,7 @@ import { GrHostMaintenance, GrStripe } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 import { isMobile } from 'react-device-detect';
 import PricingMobile from './Mobile/PricingMobile';
-import { FaEarlybirds } from 'react-icons/fa';
+import { FaLongArrowAltRight } from 'react-icons/fa';
 
 //mui
 import Alert from '@mui/material/Alert';
@@ -31,22 +31,33 @@ const Pricing = () => {
         <div className='w-full mx-auto max-w-7xl h-fit'>
           <div className='w-8/12 mx-auto flex flex-col items-center'>
             <p className='text-5xl font-medium mt-8'>Pricing</p>
-            <Alert severity='info' className='mt-2'>
-              Welcome merchants! We are very excited to finally be onboarding
-              users. Please enjoy our platform for FREE while we collect
-              feedback and develop a pricing plan.
-            </Alert>
+
+            <div className='w-full mt-4 flex justify-between'>
+              <div className='border-2 rounded w-5/12 flex items-center justify-center'>
+                <p className='text-2xl font-medium border-b border-slate-800 text-slate-800'>
+                  First product page FREE
+                </p>
+              </div>
+              <div className='w-2/12 flex flex-col items-center justify-center'>
+                <p>additional pages</p>
+                <FaLongArrowAltRight className='text-3xl' />
+              </div>
+              <div className='border-2 rounded w-5/12 flex flex-col items-center justify-center p-2'>
+                <p className='font-medium text-4xl'>
+                  <span className='text-4xl'>$3</span>/mo
+                </p>
+                <p className='font-medium text-gray-400 text-xl'>
+                  Per additional product page
+                </p>
+              </div>
+            </div>
+
             <div className='border-2 rounded w-full h-full p-4 mt-4'>
               {/* <div className='flex items-center mb-2'>
                 <p className='text-3xl font-medium'>Early bird special</p>
                 <FaEarlybirds className='text-3xl' />
               </div> */}
-              <p className='font-medium text-4xl'>
-                <span className='text-6xl'>TBD</span>/mo
-              </p>
-              <p className='font-medium text-gray-400 text-xl'>
-                Per product page
-              </p>
+
               <p className='mt-8 text-xl text-slate-800 font-medium'>
                 Platform includes:
               </p>
@@ -54,19 +65,16 @@ const Pricing = () => {
                 <p className='mt-2'>
                   - Easily launch, design, and manage product pages
                 </p>
-                <p className='mt-4'>- Import products</p>
+                <p className='mt-4'>- Offer physical or digital goods</p>
                 <p className='mt-4'>
                   - Get shipping labels & rates directly from us
                 </p>
                 <p className='mt-4'>- No transaction fees</p>
                 <p className='mt-4'>
-                  - Automated customer emailing (order confirmation, order
-                  shipped, etc.)
+                  - Automated customer emailing and order tracking
                 </p>
-                <p className='mt-4'>- Order tracking</p>
                 <p className='mt-4'>- Collect reviews from customers</p>
-                <p className='mt-4'>- Connect your own domain</p>
-                <p className='mt-4'>- 24/7 email support</p>
+                <p className='mt-4'>- Connect your own domain to pages</p>
               </div>
               <Link to='/signup' className='w-full'>
                 <button className='w-full mt-4 h-14 text-xl border-2 border-slate-800 text-slate-800 rounded hover:text-white hover:bg-slate-800'>
@@ -83,21 +91,23 @@ const Pricing = () => {
                 <GrStripe className='text-4xl' />
                 <p className='font-medium text-xl mt-2'>Secure payments</p>
                 <p className='mt-2 text-lg'>
-                  Securley accept many forms of payment using Stripe
+                  Securley accept several forms of payment using Stripe
                 </p>
               </div>
               <div className='w-4/12 border-2 mt-2 ml-2 rounded p-2'>
                 <MdMobileFriendly className='text-4xl' />
                 <p className='font-medium text-xl mt-2'>100% Mobile friendly</p>
                 <p className='mt-2 text-lg'>
-                  All pages are mobile friendly, never miss a mobile customer
+                  All pages are mobile friendly, never miss out on a mobile
+                  customer
                 </p>
               </div>
               <div className='w-4/12 border-2 mt-2 ml-2 rounded p-2'>
                 <GrHostMaintenance className='text-4xl' />
                 <p className='font-medium text-lg mt-2'>Hosted by us</p>
                 <p className='mt-2 text-lg'>
-                  Don't worry about hosting your page, we got you
+                  Never worry about hosting, pages are hosted and maintained by
+                  us
                 </p>
               </div>
             </div>
