92a3c21e5b70236f2a62a087455b5d891db49dd0
justinmann <justinmann24@gmail.com>
2023-08-22 05:52:00 -0400

    changes

--- src/components/Landing/Pricing.js
+++ src/components/Landing/Pricing.js
@@ -6,9 +6,9 @@ const Pricing = () => {
     <div className='w-full flex flex-col items-center mt-20 mb-20'>
       <p className='text-lg font-medium'>Pricing</p>
 
-      <div className='rounded bg-white border drop-shadow-lg flex flex-col p-4 items-center mt-6'>
-        <p className='font-semibold text-4xl'>1% flat fee</p>
-        <p className='font-medium text-md'>After payment processing fees</p>
+      <div className='rounded bg-white border drop-shadow-lg flex flex-col p-4 items-center mt-6 w-full'>
+        <p className='font-semibold text-3xl'>1% flat fee</p>
+        <p className='font-medium text-sm'>After payment processing fees</p>
         <p className='mt-4 font-medium'>Processing fees: 2.9% + 30 cents</p>
         <p className='font-medium text-md mt-6 w-96 text-center'>
           Never pay us on a monthly basis, we get paid only when you do. We just
